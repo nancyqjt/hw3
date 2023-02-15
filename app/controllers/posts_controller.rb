92fefede115f22931615a89df529451ad5e40198
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-02-15 08:33:16 +0000

    places semi done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+    def show
+        @post = Post.find_by({ "id" => params["id"] })
+        @place = Place.find_by({ "id" => @post["place_id"] })
+      end
+    
+    def new
+        @post = Post.new
+        @post["place_id"] = params["place_id"]
+    end
+    
+    def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post["date"] = params["post"]["date"]
+        @post.save
+        redirect_to "/places/#{@post["place_id"]}"
+    end
 end
