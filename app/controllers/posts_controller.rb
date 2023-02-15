ccc4f3a27882b45179e025f048599131421e5fe0
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-02-15 08:38:46 +0000

    posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
     def show
         @post = Post.find_by({ "id" => params["id"] })
         @place = Place.find_by({ "id" => @post["place_id"] })
-      end
+    end
     
     def new
         @post = Post.new
